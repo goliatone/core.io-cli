214033a23cc9af33c2868a99334d22dcfc59b390
goliatone <burgosemi@gmail.com>
2017-10-28 23:02:49 -0400

    Add initial commands

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-
+const Shuttle = require('core.io-cli-local-env/commands');
+const Generator = require('simple-project-generator/commands');
 
 module.exports.attach = function(prog) {
-
+    Shuttle.attach(prog, 'shuttle');
+    Generator.attach(prog, 'generator');
 };
