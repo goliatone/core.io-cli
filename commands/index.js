0eaa61541e84d2d33993bb8f02193ebb95a258a1
goliatone <burgosemi@gmail.com>
2017-10-29 00:00:41 -0400

    add schema generator cmd

--- commands/index.js
+++ commands/index.js
@@ -2,8 +2,10 @@
 
 const Shuttle = require('core.io-cli-local-env/commands');
 const Generator = require('simple-project-generator/commands');
+const Schema = require('waterline-to-json-schema/commands');
 
 module.exports.attach = function(prog) {
     Shuttle.attach(prog, 'shuttle');
     Generator.attach(prog, 'generator');
+    Schema.attach(prog, 'schema');
 };
