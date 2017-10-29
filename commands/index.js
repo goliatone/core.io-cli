62a17f20e011028fe4511f629214803eee12fbd0
goliatone <burgosemi@gmail.com>
2017-10-29 00:23:03 -0400

    Add scaffold sub command

--- commands/index.js
+++ commands/index.js
@@ -3,9 +3,11 @@
 const Shuttle = require('core.io-cli-local-env/commands');
 const Generator = require('simple-project-generator/commands');
 const Schema = require('waterline-to-json-schema/commands');
+const Scaffold = require('core.io-view-generator/commands');
 
 module.exports.attach = function(prog) {
     Shuttle.attach(prog, 'shuttle');
     Generator.attach(prog, 'generator');
     Schema.attach(prog, 'schema');
+    Scaffold.attach(prog, 'scaffold');
 };
