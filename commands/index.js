b296eed881663d7aba9358319b9a7047594d2387
goliatone <burgosemi@gmail.com>
2018-01-14 01:12:16 -0500

    add install command

--- commands/index.js
+++ commands/index.js
@@ -5,26 +5,35 @@ const Generator = require('simple-project-generator/commands');
 const Schema = require('waterline-to-json-schema/commands');
 const Scaffold = require('core.io-view-generator/commands');
 
-const Repl = require('./repl');
 const Run = require('./run');
+const Repl = require('./repl');
+const Install = require('./install');
 
 /**
  * Attach commands to given application context,
- * if a `namespace` is given then commands will 
+ * if a `namespace` is given then commands will
  * be added as sub-commands.
  */
-module.exports.attach = function $attach(app, namespace=false) {
-    
+module.exports.attach = function $attach(app, namespace = false) {
     const context = {
         namespace,
         prog: app.prog
     };
+
+    Install.attach(context);
+
+    Run.attach(context);
     
+    Repl.attach(context);
+
+    /**
+     * We should pull this list dynamically
+     * from our metadata file.
+     */
     Shuttle.attach(app, 'shuttle');
     Generator.attach(app, 'generator');
     Schema.attach(app, 'schema');
     Scaffold.attach(app, 'scaffold');
 
-    Run.attach(context);
-    Repl.attach(context);
+    
 };
